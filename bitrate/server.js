102a451690abd8739ff4fdbaf59f648d84c35410
davidoppen <33268369+davidoppen@users.noreply.github.com>
2018-04-11 12:00:11 +0200

    Fehlermeldung und Ergebnis erstellt

--- bitrate/server.js
+++ bitrate/server.js
@@ -15,3 +15,28 @@ const port = 3000;
 app.listen(port, function (){
    console.log('listening on port ' + port);
 });
+
+app.get('/', (request, response) => {
+   response.sendFile(__dirname + '/index.html');
+});
+
+app.post('/calculate', (request, response) => {
+   const bitrate = request.body['bitrate'];
+   const duration = request.body['duration'];
+
+   if(isNaN(bitrate) || isNaN(duration || bitrate < 0 || duration < 0)){
+      console.log('Bitte Eingabe überprüfen');
+      response.render('error',{
+         'bitrate': bitrate,
+         'duration': duration,
+         'message': 'Bitte Eingabe überprüfen'
+      });
+   } else {
+      console.log('bitrate: ' + bitrate + ' duration: ' + duration);
+      response.render('result', {
+         'bitrate': bitrate,
+         'duration': duration,
+         'result': bitrate * duration
+      });
+   }
+});
